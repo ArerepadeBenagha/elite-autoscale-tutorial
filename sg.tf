d181ba01e839250c72dc0beaa178b5cc721e5dd2
ArerepadeB <aredelulu@gmail.com>
2021-05-11 17:55:51 -0500

    setup dynamic ingress and backend

--- sg.tf
+++ sg.tf
@@ -1,46 +1,31 @@
-resource "aws_security_group" "myinstance" {
+resource "aws_security_group" "elb-securitygroup" {
+  name = "elb"
   vpc_id      = aws_vpc.main.id
-  name        = "myinstance"
   description = "security group for my instance"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      cidr_blocks = ingress.value
+      protocol    = "tcp"
+    }
   }
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.elb-securitygroup.id]
-  }
-  tags = local.common_tags
 }
 
-resource "aws_security_group" "elb-securitygroup" {
+resource "aws_security_group" "myinstance" {
+  name = "myinstance"
   vpc_id      = aws_vpc.main.id
-  name        = "elb"
-  description = "security group for load balancer"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  description = "security group for my instance"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      cidr_blocks = ingress.value
+      protocol    = "tcp"
+    }
   }
-  tags = local.common_tags
 }
--- vars.tf
+++ vars.tf
@@ -13,9 +13,20 @@ variable "path_to_public_key" {
   default = "mykey.pub"
 }
 
-variable "amis" {
-  type = map(string)
+# variable "amis" {
+#   type = map(string)
+#   default = {
+#     us-east-1 = "ami-06b6de9dbf2dbf9b7"
+#   }
+# }
+
+variable "ports" {
+  type = map(list(string))
   default = {
-    us-east-1 = "ami-062014dc3e1c37615"
+    "22"   = [ "0.0.0.0/0" ]
+    "443"  = [ "0.0.0.0/0" ]
+    "80"   = [ "0.0.0.0/0" ]
+    "8080" = [ "0.0.0.0/0" ]
+    "9090" = [ "0.0.0.0/0" ]
   }
 }
